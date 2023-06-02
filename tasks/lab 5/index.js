c73494113caf0ccdad5e3cbe71b5eaf020a17fa7
Oleg Lialin <lyalin.pa1nt3r@gmail.com>
2023-06-02 18:50:42 +0400

    Update index.js

--- tasks/lab 5/index.js
+++ tasks/lab 5/index.js
@@ -1,1 +1,37 @@
-﻿
+﻿class Lab5 {
+    constructor() {
+        this.breakpoints = /\_|\.|\,|\;|\:|\\|\!|\/|\?|\ /g;
+        this.vowels = /[aeiouаеёиоуыэюя]/gi;
+    }
+
+    isLatinOnly(text) {
+        return /^[A-Za-z]*$/.test(text);
+    }
+
+    sortByLength(array) {
+        array.sort((a, b) => {
+            return b.length - a.length;
+        });
+    }
+
+    first() {
+        const input = prompt("текст: ");
+        console.log(input)
+        let text = input.split(/\_|\.|\,|\;|\:|\\|\!|\/|\?|\ /g).filter(str => str !== '');
+        console.log(text);
+        this.sortByLength(text);
+        console.log(text[0]);
+        const noVowels = text[0].replace(/[aeiouаеёиоуыэюя]/gi, '');
+        console.log(noVowels);
+        const latins = [];
+        text.forEach(word => {
+            if (this.isLatinOnly(word)) {
+                latins.push(word);
+            }
+        });
+        console.log(latins);
+    }
+    second(){
+        
+    }
+}
